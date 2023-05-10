35dee0606c22cc18d9a582c74f3c3488993f514f
Mutesi <ktesialine@gmail.com>
2023-05-10 17:12:21 +0200

    fix:page in center

--- src/component/layouts/MainLayout.tsx
+++ src/component/layouts/MainLayout.tsx
@@ -2,7 +2,7 @@ import { Footer, Header } from "../common";
 
 const MainLayout = ({ children }: any) => {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Header />
       <div className="min-h-screen p-2">{children}</div>
       <Footer />
